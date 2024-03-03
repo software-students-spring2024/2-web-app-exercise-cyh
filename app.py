38077f294b76630aa361e957a7c9b0fbe548422d
YY35n <yinyiwen3589@gmail.com>
2024-03-03 16:06:58 -0500

    add photo

--- app.py
+++ app.py
@@ -3,15 +3,28 @@ from pymongo import MongoClient
 from bson.objectid import ObjectId
 import os
 from dotenv import load_dotenv
+from werkzeug.utils import secure_filename
+
 
 load_dotenv()
 
 app = Flask(__name__)
+app.config['IMAGE_UPLOADS'] = 'path/to/save/photos'  # Update this path
+app.config['ALLOWED_IMAGE_EXTENSIONS'] = ['PNG', 'JPG', 'JPEG', 'GIF']
 
 mongo_uri = os.getenv("MONGO_URI")
 mongo_dbname = os.getenv("MONGO_DBNAME")
 client = MongoClient(mongo_uri)
 db = client[mongo_dbname]
+
+def allowed_image(filename):
+    if not "." in filename:
+        return False
+    ext = filename.rsplit(".", 1)[1]
+    if ext.upper() in app.config['ALLOWED_IMAGE_EXTENSIONS']:
+        return True
+    else:
+        return False
 
 @app.route("/")
 def home():
@@ -31,6 +44,14 @@ def add_recipe():
             "ingredients": request.form.get("ingredients"),
             "steps": request.form.get("steps")
         }
+
+        if 'photo' in request.files:
+            photo = request.files['photo']
+            if photo.filename != '' and allowed_image(photo.filename):
+                filename = secure_filename(photo.filename)
+                photo.save(os.path.join(app.config['IMAGE_UPLOADS'], filename))
+                recipe['photo'] = filename
+
         db.recipes.insert_one(recipe)
         return redirect(url_for("home"))
     return render_template("add_recipe.html")
@@ -44,6 +65,14 @@ def edit_recipe(recipe_id):
             "ingredients": request.form.get("ingredients"),
             "steps": request.form.get("steps")
         }
+
+        if 'photo' in request.files:
+            photo = request.files['photo']
+            if photo.filename != '' and allowed_image(photo.filename):
+                filename = secure_filename(photo.filename)
+                photo.save(os.path.join(app.config['IMAGE_UPLOADS'], filename))
+                updated_recipe['photo'] = filename
+
         db.recipes.update_one({"_id": ObjectId(recipe_id)}, {"$set": updated_recipe})
         return redirect(url_for("recipe_detail", recipe_id=recipe_id))
     return render_template("edit_recipe.html", recipe=recipe)
@@ -63,6 +92,6 @@ def search_recipes():
 def profile():
     # Placeholder for actual user profile data
     return render_template("profile.html")
-
+   
 if __name__ == "__main__":
     app.run(debug=True, port=os.getenv("FLASK_PORT", 5000))
